a95dadaa7f3ad73a4c5a640833a4e722baedfdd2
BrianU <brian.ulatowski@gmail.com>
2020-02-15 13:46:06 -0800

    add images folder with images; update css to display images; add classes to main div and h1

--- assets/js/toggleScript.js
+++ assets/js/toggleScript.js
@@ -1,10 +1,11 @@
+var slider = document.querySelector('.slider');
 var ball = document.querySelector('.ball');
 var eatInText = document.querySelector('.eatIn');
 var eatOutText = document.querySelector('.eatOut');
-var body = document.querySelector('body');
-ball.addEventListener('click', () => {
+var mainWrapper = document.querySelector('.mainWrapper');
+slider.addEventListener('click', () => {
     ball.classList.toggle('right');
-    eatInText.classList.toggle('active')
+    eatInText.classList.toggle('active');
     eatOutText.classList.toggle('active');
-    body.classList.toggle('blue')
+    mainWrapper.classList.toggle('blue');
 })
