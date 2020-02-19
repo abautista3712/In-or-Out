3ddea515e3f7c502cabf4ea9b9f0f6feca480501
BrianU <brian.ulatowski@gmail.com>
2020-02-18 19:51:59 -0800

    combined script.js and togglescript.js into just script.js

--- assets/js/toggleScript.js
+++ assets/js/toggleScript.js
@@ -1,30 +1,4 @@
-var slider = document.querySelector('.slider');
-var ball = document.querySelector('.ball');
-var eatInText = document.querySelector('.eatIn');
-var eatOutText = document.querySelector('.eatOut');
-var mainWrapper = document.querySelector('.mainWrapper');
-var addLocationInput = 0;
 
-$(document).ready(function() {
-    $('.inputLocation').remove();
-})
-
-slider.addEventListener('click', () => {
-    ball.classList.toggle('right');
-    eatInText.classList.toggle('active');
-    eatOutText.classList.toggle('active');
-    mainWrapper.classList.toggle('blue');
-    
-    if (addLocationInput < 1) {
-        addLocationInput++;
-        //adds location input and button when clicked on eat out
-        $('.searchBox').append('<input class="inputLocation" type="text" placeholder="Enter city..." name="input[]"><button id="location" class="locationButton">City</button>')
-    } else if (addLocationInput = 1) {        
-        //removes the location input and button when clicked back to eat in
-        $('.inputLocation').remove();
-        $('.locationButton').remove();
-        addLocationInput--;
-    };
 
     // // if (ball.classList.toggle('right')) {
     // var locationInput = document.createElement('input');
@@ -32,7 +6,7 @@ slider.addEventListener('click', () => {
     //     locationInput.setAtrribute('value', type);
     //     locationInput.setAtrribute('location', type);
     // // }
-})
+
 
 // $('.slider').click(function () {
 //     $('.searchBox').append('<input type="text" name="input[]"><button id="location">Location</button>')
