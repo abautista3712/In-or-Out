ef59e23e0b74d3aac6de81afae4c2ff7737ff2a6
Abelard Bautista <abelardbautista@gmail.com>
2020-02-19 22:16:59 -0800

    Consolidated eating out pages.

--- assets/js/eat_out_content.js
+++ assets/js/eat_out_content.js
@@ -1,23 +1,22 @@
 var location2 = "Los Angeles";
 $.ajax({
   url:
-    "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=" + location2,
+    "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=" +
+    location2,
   method: "GET",
   headers: {
     Authorization:
       "Bearer roEn_ezE99s_UqG2kH4-r-nbOAMwFTCCGgZRqUz2zXswR_0l4zdPM4Kcyrb_39E1vl96VofmaT92syqs1RSvkoqdS0bf_3h1DCykXbLjOlEUbUEAsT3CvBFdX0pIXnYx"
   }
-})
-.then(function(response) {
-    console.log(response.businesses);
-    var responseID = response.businesses[0].id;
+}).then(function(response) {
+  console.log(response.businesses);
+  var responseID = response.businesses[0].id;
 
+  console.log(response.businesses[0]);
+  $("#dataCoordinates").text(response.businesses[0].coordinates);
 
-    console.log(response.businesses[0]);
-    $("#dataCoordinates").text(response.businesses[0].coordinates);
-
-// // Iterative for-loop used to create results boxes
-for (a = 1; a < 6; a++) {
+  // // Iterative for-loop used to create results boxes
+  for (a = 1; a < 6; a++) {
     // Materialize Row
     $("<div>", { id: "listResultsBox" + a, class: "row" }).appendTo(
       "#eatOutContent"
@@ -52,34 +51,79 @@ for (a = 1; a < 6; a++) {
       .text("display_phone")
       .appendTo("#dataResultsWrapper" + a);
 
-
     // Wrapper used to restrict sizing/positioning on recipe link
     $("<div>", { id: "dataLinktoWebsite" + a, class: "col s12" }).appendTo(
-      "#dataResultsWrapper" + a);
+      "#dataResultsWrapper" + a
+    );
 
     // Data: Restaurant
     $("<a>", { id: "dataLinkToRestaurant" + a })
       .text("url")
       .appendTo("#dataLinktoWebsite" + a);
 
-          // Data: Coordinates
+    // Data: Coordinates
     $("<div>", { id: "coordinates" + a, class: "col s12" })
-    .text("latitude, longitude")
-    .appendTo("#dataResultsWrapper" + a);
+      .text("latitude, longitude")
+      .appendTo("#dataResultsWrapper" + a);
+  }
+});
 
-    }}
-    );    
-
-  //  Google Maps
+//  Google Maps
 // Initialize and add the map
 function initMap() {
-    // The location of Uluru
-    var uluru = {lat: -25.344, lng: 131.036};
-    
-    // The map, centered at Uluru
-    var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 4, center: uluru});
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({position: uluru, map: map});
+  // The location of Uluru
+  var uluru = { lat: -25.344, lng: 131.036 };
+
+  // The map, centered at Uluru
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: uluru
+  });
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({ position: uluru, map: map });
+}
+
+// //function myMap() {
+//     var mapProp= {
+//       center:new google.maps.LatLng(51.508742,-0.120850),
+//       zoom:5,
+//     };
+//     var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+//     }
+
+//     function init() {
+//         var input = document.getElementById('locationTextField');
+//         var autocomplete = new google.maps.places.Autocomplete(input);
+//     }
+
+//     google.maps.event.addDomListener(window, 'load', init);
+
+//     //console.log(init)
+
+//Yelp
+var location2 = "Los Angeles";
+$.ajax({
+  url:
+    "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=" +
+    location2,
+  method: "GET",
+  headers: {
+    Authorization:
+      "Bearer roEn_ezE99s_UqG2kH4-r-nbOAMwFTCCGgZRqUz2zXswR_0l4zdPM4Kcyrb_39E1vl96VofmaT92syqs1RSvkoqdS0bf_3h1DCykXbLjOlEUbUEAsT3CvBFdX0pIXnYx"
   }
- 
+}).then(function(response) {
+  console.log(response.businesses);
+  var responseID = response.businesses[0].id;
+
+  console.log(response.businesses[0].image_url);
+  $("#imageResults").attr("src", response.businesses[0].image_url);
+  $("#dataName").text(response.businesses[0].name);
+  $("#dataDirections").text(response.businesses[0].location.display_address);
+  $("#dataPhone").text(response.businesses[0].display_phone);
+
+  $("#dataURLtoWebsite").attr("href", response.businesses[0].url);
+  $("#dataURLtoWebsite").text(response.businesses[0].url);
+  console.log(response.businesses[0].url);
+
+  var Data = $("#");
+});
