4140d4d6573fc41b08427fd0c1e68e370059ef12
Abelard Bautista <abelardbautista@gmail.com>
2022-01-23 00:02:30 -0800

    Found alternative cors-proxy.

--- assets/js/eat_out_content.js
+++ assets/js/eat_out_content.js
@@ -86,7 +86,7 @@ function pingMapAndZoom() {
 
 jQuery.ajaxPrefilter(function (options) {
   if (options.crossDomain && jQuery.support.cors) {
-    options.url = "https://cors-anywhere.herokuapp.com/" + options.url;
+    options.url = "https://corsanywhere.herokuapp.com/" + options.url;
   }
 });
 
@@ -96,7 +96,7 @@ function getDataAndAttach(indexID, targetRow) {
   var queryTerm = localStorage.getItem("foodItem");
   $.ajax({
     url:
-      "https://still-shore-42578.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=" +
+      "https://corsanywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=" +
       location +
       "&term=" +
       queryTerm,
@@ -186,7 +186,7 @@ function getMap() {
   var queryTerm = localStorage.getItem("foodItem");
   $.ajax({
     url:
-      "https://still-shore-42578.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=" +
+      "https://corsanywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=" +
       location +
       "&term=" +
       queryTerm,
